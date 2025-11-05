b1e9500473216289dec25566c969cf98de38f3d5
Peter Simon <124758601+petereugenesimon@users.noreply.github.com>
2025-11-05 09:13:43 -0600

    Add files via upload

---
+++ 2025/October/101525/101525_TB_NYY.sql
@@ -0,0 +1,69 @@
+-- LAHMAN DB DOES NOT CONTAIN ANY 2025 RECORDS
+
+
+-- Query franchise & team IDs
+SELECT DISTINCT(tf.franchID), tf.franchName, t.teamID, t.name
+FROM dbo.TeamsFranchises AS tf
+INNER JOIN dbo.Teams AS t
+	ON tf.franchID = t.franchID;
+
+
+-- Create temp table for all players with batting records for TBD franchise
+SELECT DISTINCT(bt.playerID), p.nameFirst, p.nameLast
+INTO #tbd_bat
+FROM dbo.Batting AS bt
+INNER JOIN dbo.People AS p
+	ON bt.playerID = p.playerID
+WHERE bt.teamID = 'TBA';
+
+
+-- Create temp table for all players with pitching records for TBD franchise
+SELECT DISTINCT(pt.playerID), p.nameFirst, p.nameLast
+INTO #tbd_pit
+FROM dbo.Pitching AS pt
+INNER JOIN dbo.People AS p
+	ON pt.playerID = p.playerID
+WHERE pt.teamID = 'TBA';
+
+
+-- Create temp table joining every recorded player for TBD franchise
+SELECT *
+INTO #tbd
+FROM #tbd_bat
+UNION
+SELECT *
+FROM #tbd_pit;
+
+
+-- Create temp table for all players with batting records for NYY franchise
+SELECT DISTINCT(bt.playerID), p.nameFirst, p.nameLast
+INTO #nyy_bat
+FROM dbo.Batting AS bt
+INNER JOIN dbo.People AS p
+	ON bt.playerID = p.playerID
+WHERE bt.teamID IN ('BLA', 'NYA');
+
+
+-- Create temp table for all players with pitching records for NYY franchise
+SELECT DISTINCT(pt.playerID), p.nameFirst, p.nameLast
+INTO #nyy_pit
+FROM dbo.Pitching AS pt
+INNER JOIN dbo.People AS p
+	ON pt.playerID = p.playerID
+WHERE pt.teamID IN ('BLA', 'NYA');
+
+
+-- Create temp table joining every recorded player for NYY franchise
+SELECT *
+INTO #nyy
+FROM #nyy_bat
+UNION
+SELECT *
+FROM #nyy_pit;
+
+
+-- Query all players who played for TBD & NYY franchises
+SELECT DISTINCT(tbd.playerID), tbd.nameFirst, tbd.nameLast
+FROM #tbd AS tbd
+INNER JOIN #nyy AS nyy
+	ON tbd.playerID = nyy.playerID;
